1e8d930b223d451506a65a0496e833a1f8e91f98
ShylkoVad <shylkovad@gmail.com>
2023-02-08 11:29:40 +0300

    IF

---
+++ src/by/teachmeskills/homeworks/uroki/urokIf.java
@@ -0,0 +1,12 @@
+package by.teachmeskills.homeworks.uroki;
+
+public class urokIf {
+    public static void main(String[] args) {
+        int x = 10;
+
+        if (x < 20) {
+            System.out.println("Это оператор if");
+        }
+
+    }
+}
