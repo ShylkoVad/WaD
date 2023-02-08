96534d1c6fced65a04ba48d9d2e5991b14d46008
ShylkoVad <shylkovad@gmail.com>
2023-02-08 11:29:40 +0300

    0000

---
+++ src/by/teachmeskills/homeworks/hw_10022023/WorksGit.java
@@ -0,0 +1,10 @@
+package by.teachmeskills.homeworks.hw_10022023;
+
+public class WorksGit {
+    public static void main(String[] args) {
+            int a = 200;
+            int b = 143;
+            int sum = a + b;
+            System.out.printf("Sum of numbers " + a + " and " + b + " is " + sum);
+        }
+}
