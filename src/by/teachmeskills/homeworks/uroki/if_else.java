8202f0c69fc6cc1ad1284acfe2b5b44f87fc699e
ShylkoVad <shylkovad@gmail.com>
2023-02-08 11:29:40 +0300

    ELSE

--- src/by/teachmeskills/homeworks/uroki/if_else.java
+++ src/by/teachmeskills/homeworks/uroki/if_else.java
@@ -1,13 +1,14 @@
 package by.teachmeskills.homeworks.uroki;
 
-public class urokIf {
+public class if_else {
     public static void main(String[] args) {
-        int x = 10;
-        int y = 20;
-
+        int x = 30;
+        int y = 10;
         if (x < y) {
             System.out.println("Это оператор if");
         }
-
+        else {
+            System.out.println("Это оператор else");
+        }
     }
 }
