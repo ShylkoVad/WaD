6ec7c35778731042681cf87a5e1030f69ecc020b
ShylkoVad <shylkovad@gmail.com>
2023-02-08 23:15:05 +0300

    Switch

---
+++ src/by/teachmeskills/homeworks/hw_17022023/Switch.java
@@ -0,0 +1,34 @@
+package by.teachmeskills.homeworks.hw_17022023;
+
+public class Switch {
+    public static void main(String[] args) {
+        int n = 10;
+
+
+        switch (n){
+            case 1, 8:
+                System.out.println("Сегодня понедельник");
+                break;
+            case 2, 9:
+                System.out.println("Сегодня вторник");
+                break;
+            case 3:
+                System.out.println("Сегодня среда");
+                break;
+            case 4:
+                System.out.println("Сегодня четверг");
+                break;
+            case 5:
+                System.out.println("Сегодня пятница");
+                break;
+            case 6:
+                System.out.println("Сегодня суббота");
+                break;
+            case 7:
+                System.out.println("Сегодня воскресенье");
+                break;
+            default:
+                System.out.println("Не корректно введено число недели");
+        }
+    }
+}
