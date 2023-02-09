c32da47a7156dddf15133c7c473c728e841604fd
ShylkoVad <shylkovad@gmail.com>
2023-02-09 10:28:29 +0300

    loop

--- src/by/teachmeskills/homeworks/hw_17022023/Loop.java
+++ src/by/teachmeskills/homeworks/hw_17022023/Loop.java
@@ -2,6 +2,16 @@ package by.teachmeskills.homeworks.hw_17022023;
 
 public class Loop {
     public static void main(String[] args) {
+        int line = 4;
+        int column = 5;
+
+        for (int y = 0; y < line; ++y) {
+            for (int x = 0; x < column; ++x) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+
 
     }
 }
