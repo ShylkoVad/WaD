344ea0c4bd7d93f2525112dd5d4a4c5243a7d4d0
ShylkoVad <shylkovad@gmail.com>
2023-02-10 12:49:02 +0300

    2

--- src/by/teachmeskills/homeworks/hw_17022023/Massiv_2.java
+++ src/by/teachmeskills/homeworks/hw_17022023/Massiv_2.java
@@ -4,27 +4,29 @@ import java.util.Arrays;
 
 public class Massiv_2 {
     public static void main(String[] args) {
-        double[] randoms = new double[]{10, 2, 3, 4, 5, 11}; // иницилизация
+        int[] randoms = new int[]{10, 2, 3, 4, 5, 11}; // иницилизация
+
+
+//        int n = 6;
+//        int[] randoms = new int[n];
         System.out.println(Arrays.toString(randoms));
 
-        boolean sorted = false;
-        int temp;
+//        for (int i = 0; i < randoms.length; i++) {
+//            randoms[i] = Math.random(); // в каждый i элемент массива кладем рандомное значение
+//            System.out.print(randoms[i] + " ");
+//        }
 
-        while (!sorted){
-            sorted = true;
-            for (int i = 0; i < randoms.length - 1; i ++) {
-                if (randoms[i] > randoms[i + 1]) {
-                    temp = (int) randoms[i];
-                    randoms[i] = randoms[i + 1];
-                    randoms[i + 1] = temp;
-                    sorted = false;
+       int temp;
 
-
+        for (int i = 0; i < randoms.length - 1; i++){
+                for (int j = 0; j < randoms.length - 1 - i; j ++) {
+                if (randoms[j] > randoms[j + 1]) {
+                    temp = randoms[j];
+                    randoms[j] = randoms[j + 1];
+                    randoms[j + 1] = temp;
 
                 }
             }
-
-
         }
         System.out.println(Arrays.toString(randoms));
 
