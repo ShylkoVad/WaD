3b8746f305f8331ae23a6015f849edcac87e27ac
ShylkoVad <shylkovad@gmail.com>
2023-02-11 12:41:17 +0300

    11/02 - 12:41

--- src/by/teachmeskills/homeworks/hw_17022023/Massiw_torn.java
+++ src/by/teachmeskills/homeworks/hw_17022023/Massiw_torn.java
@@ -3,25 +3,27 @@ package by.teachmeskills.homeworks.hw_17022023;
 public class Massiw_torn {
     public static void main(String[] args) {
 
-        int line = (int) (1 + Math.random() * 9 ); //рандомные числа от (0 до 10] - количество строк
-        int column = (int) (1 + Math.random() * 9 ); //рандомные числа от (0 до 10] - то число столбцов которое нельзя привышать
+        int line = (int) (1 + Math.random() * 10 ); //рандомные числа от (0 до 10] - количество строк
+        int column = (int) (1 + Math.random() * 10 ); //рандомные числа от (0 до 10] - то число столбцов для инициализации массива
 
         System.out.println("Рандомное число строк матрицы - " + line);
-        System.out.println("Рандомное число столбцов матрицы - " + column);
-
+        System.out.println("Рандомное максимальное число столбцов матрицы - " + column);
+        System.out.println();
 
         int[][] randoms = new int[line][column]; // иницилизация массива
 
-        for (int i = 0; i < randoms.length; i++) {
-//            int column_line = 3;
-            int column_line = (int) (1 + Math.random() * 9); //рандомные числа от (0 до 10] - количество столбцов в строке
-            if (column_line <= column) {
-                for (int j = 0; j < column_line; j++) {
+        for (int i = 0; i < randoms.length; i++) { // цикл по строкам
+
+            int column_line = (int) (1 + Math.random() * 10); //рандомные числа от (0 до 10] - количество столбцов в строке
+
+            while (column_line > column) { // цикл если число столбцов в строке превышает инициализированное число столбцов в матрице
+                column_line = (int) (1 + Math.random() * 10); //рандомные числа от (0 до 10] - количество столбцов в строке
+            }
+               for (int j = 0; j < column_line; j++) {
                     randoms[i][j] = (int) (Math.random() * (200 + 1) - 100); //рандомные числа от -100 до 100
                     System.out.print(randoms[i][j] + " ");
                 }
 
-            }
             System.out.println();
         }
 
