9bac1d203e1805dff04935bbff83dabe774c8e8e
ShylkoVad <shylkovad@gmail.com>
2023-02-16 15:35:33 +0300

    16/02/2023 15:35 - RecursionArray

--- src/by/teachmeskills/homeworks/hw_24022023/recursion/RecursionSorting.java
+++ src/by/teachmeskills/homeworks/hw_24022023/recursion/RecursionSorting.java
@@ -6,45 +6,23 @@ public class RecursionSorting {
 
         int numberA;
         int numberB;
-        int quantityNumber;
-<<<<<<< HEAD
-=======
-        int summ = 0;
->>>>>>> 95fc942 (16/02/2023 16:02 - RecursionArray)
+        int sum = 0;
 
 
         numberA = (int) (Math.random() * (20 + 1) - 10); //рандомные числа от -100 до 100
         numberB = (int) (Math.random() * (20 + 1) - 10); //рандомные числа от -100 до 100
-        quantityNumber = Math.abs((numberA - numberB) + 1);
 
         System.out.println("Число А составляет: " + numberA);
-        System.out.println("Число B составляет: " +numberB);
-<<<<<<< HEAD
-        System.out.println("Количество чисел: " + quantityNumber);
-
-            }
-//            static int sorting (int i) {
-//
-//
-//
-//
-//            }
-=======
-//        System.out.println("Количество чисел: " + quantityNumber);
+        System.out.println("Число B составляет: " + numberB);
 
         if (numberA < numberB) {
-            for (int i = 0; numberA < numberB ; i ++) {
-                summ = summ +  numberA + i;
-                System.out.println(summ);
-
+            for (int i = 0; numberA < numberB; i++) {
+                sum = sum + numberA + i;
+                System.out.println(sum);
             }
-
         } else {
             System.out.println("DA");
         }
 
-            }
-
-
->>>>>>> 95fc942 (16/02/2023 16:02 - RecursionArray)
+    }
 }
