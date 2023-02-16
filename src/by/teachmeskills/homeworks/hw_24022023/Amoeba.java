5073e9ad4d6d44504f62195b735a25579901376a
ShylkoVad <shylkovad@gmail.com>
2023-02-16 11:29:09 +0300

    16/02/2023 - Amoeba

--- src/by/teachmeskills/homeworks/hw_24022023/Amoeba.java
+++ src/by/teachmeskills/homeworks/hw_24022023/Amoeba.java
@@ -1,4 +1,18 @@
 package by.teachmeskills.homeworks.hw_24022023;
 
 public class Amoeba {
+    public static void main(String[] args) {
+
+        int time = 0;
+        int hour = 3; // время деления
+        int cell = 2; // количество клеток при одном делении
+        int amoeba = 1; // первоночальное количество амеб
+
+        for (int i = 0; i < 24; i = i + hour) {
+
+            amoeba = amoeba * cell;
+            time = time + hour;
+            System.out.printf("При делении амебы в %d часа их количество составит %d\n", time, amoeba);
+        }
+    }
 }
