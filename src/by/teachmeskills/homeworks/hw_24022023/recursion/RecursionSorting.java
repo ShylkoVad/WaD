e4c160635d1fc9300aa942568a6410ba0699e6ce
ShylkoVad <shylkovad@gmail.com>
2023-02-16 22:03:12 +0300

    16/02/2023 - 22:03

--- src/by/teachmeskills/homeworks/hw_24022023/recursion/RecursionSorting.java
+++ src/by/teachmeskills/homeworks/hw_24022023/recursion/RecursionSorting.java
@@ -1,33 +1,42 @@
 package by.teachmeskills.homeworks.hw_24022023.recursion;
 
 public class RecursionSorting {
-
     public static void main(String[] args) {
 
-        int numberA = 0;
-        int numberB = 0;
-        int summ = 0;
+        int numberA;
+        int numberB;
 
-//        numberA = (int) (Math.random() * (20 + 1) - 10); //рандомные числа от -100 до 100
-//        numberB = (int) (Math.random() * (20 + 1) - 10); //рандомные числа от -100 до 100
+        numberA = (int) (Math.random() * (20 + 1) - 10); //рандомные числа от -100 до 100
+        numberB = (int) (Math.random() * (20 + 1) - 10); //рандомные числа от -100 до 100
 
         System.out.println("Число А составляет: " + numberA);
         System.out.println("Число B составляет: " + numberB);
+        System.out.print(" " + sorting(numberA, numberB));
 
-        if (numberA < numberB) {
-            for (int i = 0; (numberA + i) <= numberB; i++) {
-                summ = numberA + i;
-                System.out.print(" " + summ);
-            }
-            } else if (numberA > numberB) {
-            for (int i = 0; (numberB + i) <= numberA; i++) {
-                summ = numberA - i;
-                System.out.print(" " + summ);
-            }
-        } else {
-           System.out.println("Число A равно числу B");
     }
 
+    static int sorting(int numberA, int numberB) {
 
+        if (numberA < numberB) {
+
+            int sum = sorting(numberA, numberB - 1);
+            System.out.print(" " + sum);
+
+        } else if (numberA > numberB) {
+
+            int sum = sorting(numberA, numberB + 1);
+            System.out.print(" " + sum);
+
+        }
+
+        return numberB;
+    }
 }
-}
+
+
+
+
+
+
+
+
