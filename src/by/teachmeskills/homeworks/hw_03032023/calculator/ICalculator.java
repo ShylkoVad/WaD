9308c0832aebb748e5e129ac441c47a60d4178b8
ShylkoVad <shylkovad@gmail.com>
2023-03-02 12:13:11 +0300

    git merge branch_24022023

--- src/by/teachmeskills/homeworks/hw_03032023/calculator/ICalculator.java
+++
@@ -1,8 +0,0 @@
-package by.teachmeskills.homeworks.hw_03032023.calculator;
-
-public interface ICalculator {
-    default int sum(double a, double b) {
-        return sum(a, b);
-
-    }
-}
--- src/by/teachmeskills/homeworks/hw_03032023/calculator/performance.java
+++
@@ -1,4 +0,0 @@
-package by.teachmeskills.homeworks.hw_03032023.calculator;
-
-public class performance {
-}
