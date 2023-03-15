13a07c588abffce78a011c7db8b6a2178b7037d6
ShylkoVad <shylkovad@gmail.com>
2023-03-15 13:41:23 +0300

    15/03/2023 - 13:41

--- src/by/teachmeskills/homeworks/hw_17032023/text/Main.java
+++ src/by/teachmeskills/homeworks/hw_17032023/text/Main.java
@@ -1,12 +1,21 @@
 package by.teachmeskills.homeworks.hw_17032023.text;
 
-import java.io.FileWriter;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        TextFormater.returnNumberWords("Текстовый файл содержит текст, которого нет");
-        TextFormater.checkPalindromeString("Текстовый файл: содержит текст, которого нет - паап.");
 
-
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("data\\hw_1703\\OriginalText.txt"));
+             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("data\\hw_1703\\FormaterText.txt"))) {
+            String str;
+            while ((str = bufferedReader.readLine()) != null) {
+                if (TextFormater.checkPalindromeString(str) | (TextFormater.returnNumberWords(str) >= 3 & TextFormater.returnNumberWords(str) <= 5)) {
+                    bufferedWriter.write(str + "\n");
+                    bufferedWriter.flush();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/by/teachmeskills/homeworks/hw_17032023/text/TextFormater.java
+++ src/by/teachmeskills/homeworks/hw_17032023/text/TextFormater.java
@@ -1,34 +1,25 @@
 package by.teachmeskills.homeworks.hw_17032023.text;
 
 public class TextFormater {
-    public static void returnNumberWords(String str) {
+
+    static int returnNumberWords(String str) {
+
         String[] arrayText = str.split(" ");
-        if (arrayText.length >= 3 & arrayText.length <= 5) {
-            boolean numberWords = true;
-            System.out.print(numberWords);
-        } else {
-            boolean numberWords = false;
-            System.out.println(numberWords);
-        }
-        System.out.println();
+        return arrayText.length;
     }
 
-    public static void checkPalindromeString(String strPalindrome) {
+    static boolean checkPalindromeString(String str) {
 
-        strPalindrome = strPalindrome.replaceAll("\\p{P}", "");
-        strPalindrome = strPalindrome.replaceAll("  ", " ");
-        String[] arrayText = strPalindrome.split(" ");
+        str = str.replaceAll("\\p{P}", "");
+        str = str.replaceAll("  ", " ");
+        String[] arrayText = str.split(" ");
 
         for (String s : arrayText) {
             StringBuilder palindrome = new StringBuilder(s).reverse();
             if (s.equalsIgnoreCase(palindrome.toString())) {
-                boolean numberWords = true;
-                System.out.println(numberWords);
-                System.out.print(s + "; ");
-            } else {
-                boolean numberWords = false;
-                System.out.println(numberWords);
+                return true;
             }
         }
+        return false;
     }
 }
